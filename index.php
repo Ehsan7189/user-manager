d29ec9ced6f97f5b64c6c903747f7366a191393f
Ehsan7189 <ehsansayadi7189@gmail.com>
2026-09-07 11:17:32 +0330

    feat : import login corect address

--- index.php
+++ index.php
@@ -17,7 +17,7 @@
 		<nav id='navmenu' class='navmenu'>
 			<ul>
 				<li><a href='#hero' class='active'>Home</a></li>
-				<li><a href='#about'>Login</a></li>
+				<li><a href='login.php'>Login</a></li>
 				<li><a href='register.php'>Register</a></li>
 				<li><a href='#portfolio'>Portfolio</a></li>
 				<li><a href='#team'>Team</a></li>
